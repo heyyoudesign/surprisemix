7b937d7a02f495754fd5d0fc4e71ce7e13691a95
Adriano <ajaneu@gmail.com>
2023-11-09 13:00:52 +0100

    site title

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,12 +1,12 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Eleventy Excellent',
+  siteName: 'Surprise',
   siteDescription:
-    'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
-  siteType: 'Person', // schema
-  locale: 'en_EN',
-  lang: 'en',
-  skipContent: 'Skip to content',
+    'Plant based mix',
+  siteType: 'Business', // schema
+  locale: 'it_IT',
+  lang: 'it',
+  skipContent: 'Vai ai contenuti',
   author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
   authorEmail: '', // i.e. hola@lenesaile.com - email of the author
   authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
@@ -15,31 +15,31 @@ module.exports = {
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
     opengraph_default_alt:
-      'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ', // alt text for default meta image
+      'Plant based mix', // alt text for default meta image
     twitterSite: '', // i.e. @site - twitter profile of the site
     twitterCreator: '', // i.e. @author -  twitter profile of the site
     mastodonProfile: '' // i.e. https://front-end.social/@lene - url to your mastodon instance/profile
   },
   blog: {
     // this is for the rss feed
-    name: 'My great Web Development Blog',
+    name: 'Ricettario di Surprise Mix',
     description:
-      'Tell the word what you are writing about in your blog! It will show up on feed readers.'
+      'Con SURPRISE puoi preparare sostituti vegani della carne come burger, polpette e ripieni. Privo di allergeni.'
   },
   pagination: {
     itemsPerPage: 20
   },
   address: {
     // edit all presets or leave empty. They are being used in the pages for privacy.md and imprint.md
-    firma: 'Organization name',
-    street: '123 Main St.',
-    city: 'Ciudad',
-    state: 'Estado',
-    zip: '12345',
-    mobileDisplay: '+34 1234567',
-    mobileCall: ' +341234567',
-    email: 'hola@yoursite.com',
-    cif: ''
+    firma: 'Prodotti Spiga srl',
+    street: 'via Montebello, 27',
+    city: 'Milano',
+    state: 'Italy',
+    zip: '20121',
+    mobileDisplay: '+39 02 62086164',
+    mobileCall: ' +390262086164',
+    email: 'info@prodottispiga.it',
+    cif: 'IT11967140150'
   },
   menu: {
     closedText: 'Menu'
